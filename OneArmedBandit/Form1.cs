1dc5b562ee789f744464a9da27283a49fa95092f
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-10-18 11:26:33 -0400

    in progress

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -16,7 +16,7 @@ namespace OneArmedBandit
     public partial class Form1 : Form
     {
         // random number generator
-        Random randGen = new Random();  
+        Random randGen = new Random();
 
         // int value for score initialized to 10
         int score = 10;
@@ -26,21 +26,62 @@ namespace OneArmedBandit
             InitializeComponent();
         }
 
-        private void spinButton_Click(object sender, EventArgs e) 
+        private void spinButton_Click(object sender, EventArgs e)
         {
             // get random values for each reel (store each in separate int variable)
+
+            int reel1 = randGen.Next(1, 4);
+            int reel2 = randGen.Next(1, 4);
+            int reel3 = randGen.Next(1, 4);
 
 
             // check value of reel 1 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
 
+            switch (reel1)
+            {
+                case 1:
+                    pictureBox1.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
 
             // check value of reel 2 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
 
+            switch (reel2)
+            {
+                case 1:
+                    pictureBox2.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox2.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox2.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
+
             // check value of reel 3 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+           
+            switch (reel3)
+            {
+                case 1:
+                    pictureBox3.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox3.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox3.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
 
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
@@ -50,7 +91,14 @@ namespace OneArmedBandit
 
             // Use compound if statement to check if all reels are equal. 
             // If yes show "winner" statement and add 3 to score.
-            // If no show "play again" statement and subtract 1 from score.         
+            // If no show "play again" statement and subtract 1 from score.
+
+            if (reel1 == reel2 && reel2 == reel3)
+            {
+                score++;
+                scoreDisplay.Text = $"{score}";
+                outputLabel.Text = "Winner!!!";
+            }
 
 
             // if score has reached 0 display "lose" message and set button enabled property to false
